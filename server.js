66b457387e59c055377f9736ead2c1199ba07826
joshemrn <josh@emrn.ca>
2025-07-28 17:14:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import axios from "axios";
 import * as cheerio from "cheerio";
@@ -6,13 +5,18 @@ import * as cheerio from "cheerio";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Scrape mock MERX public solicitations page
 async function scrapeMerx() {
   const url = "https://www.merx.com/public/supplier/solicitations";
   const bids = [];
 
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36"
+      }
+    });
+
     const $ = cheerio.load(data);
 
     $(".solicitation .solicitation-title").each((_, el) => {
